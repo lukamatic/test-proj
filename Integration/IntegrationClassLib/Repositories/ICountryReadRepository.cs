ef6efeae2295f6f807e9e7251a8235678b772723
lukamatic <matic.ra16.2018@uns.ac.rs>
2021-11-20 22:21:01 +0100

    added other projs

---
+++ Integration/IntegrationClassLib/Repositories/ICountryReadRepository.cs
@@ -0,0 +1,10 @@
+﻿using Integration.Model;
+using Integration.Repositories.Base;
+
+namespace Integration.Repositories
+{
+    public interface ICountryReadRepository : IReadBaseRepository<int, Country>
+    {
+        public Country GetByName(string Name);
+    }
+}
